47c475555176ff095879e2e6115ded9121e6fe71
Jo <17394+JoMangee@users.noreply.github.com>
2019-12-15 15:39:00 +1300

    Fixed footer images

--- footer.php
+++ footer.php
@@ -13,16 +13,16 @@
                 <div class="rightSec">
                     <div class="socialWrapper">
                         <a class="facebook" href="http://facebook.com/1vs5i" target="_blank">
-                            <img src="images/facebook.png" alt="Facebook" />
+                            <img src="../images/facebook.png" alt="Facebook" />
                         </a>
                         <a class="twitter" href="http://twitter.com/1vs5i" target="_blank">
-                            <img src="images/twitter.png" alt="Twitter" />
+                            <img src="../images/twitter.png" alt="Twitter" />
                         </a>
                         <a class="youtube" href="https://www.youtube.com/channel/UCJxSAxlxlL9KHp7Dp9Nlu5A" target="_blank">
-                            <img src="images/youtube.png" alt="Youtube" />
+                            <img src="../images/youtube.png" alt="Youtube" />
                         </a>
                         <a class="gogetfunding" href="https://gogetfunding.com/help-a-kiwi-journalist-take-on-the-spies-1vs5i/" target="_blank">
-                            <img src="images/dollar.png" alt="Gogetfunding" />
+                            <img src="../images/dollar.png" alt="Gogetfunding" />
                         </a>
                     </div>
                     <p>&copy; Copyright 2019 | All rights reserved | Powered by BellaBook
