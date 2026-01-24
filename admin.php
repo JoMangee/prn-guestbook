a393a3842313933782c82bea5e1c76c4345bfed7
Jo <17394+JoMangee@users.noreply.github.com>
2026-01-24 16:17:48 +1300

    Fix cookie validation - use fixed secret instead of changing session_id

--- admin.php
+++ admin.php
@@ -12,7 +12,7 @@ require_once('config.php');
 
 if (isset($_COOKIE['timotheus_guestbook'])) {
 	// Security: Use SHA256 instead of MD5
-	if ($_COOKIE['timotheus_guestbook'] == hash('sha256', $admin_pass.$secret.session_id())) {
+	if ($_COOKIE['timotheus_guestbook'] == hash('sha256', $admin_pass.$secret)) {
 		if (isset($_GET['p'])) $page = $_GET['p'];
 		else $page = NULL;
 		
@@ -313,7 +313,7 @@ if (isset($_GET['p']) && $_GET['p'] == "login") {
 	} else if ($_POST['name'] == $admin_name && $_POST['pass'] == $admin_pass) {
 		// Security: Start session and use SHA256 with secure cookie flags
 		if (session_status() == PHP_SESSION_NONE) session_start();
-		setcookie('timotheus_guestbook', hash('sha256', $_POST['pass'].$secret.session_id()), time()+(31*86400), '/', '', isset($_SERVER['HTTPS']), true);
+		setcookie('timotheus_guestbook', hash('sha256', $_POST['pass'].$secret), time()+(31*86400), '/', '', isset($_SERVER['HTTPS']), true);
 		header("Location: admin.php");
 		exit;
 	} else {
